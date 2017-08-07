fdef3134c3f9bcc5a975c507d2026c981004cca0
Sivaranjith1 <ranjith1210000@gmail.com>
2017-08-07 20:47:18 +0200

    makemigrations

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class article(models.Model):
+    image_title = models.CharField(max_length=100)
+    image_text = models.CharField(max_length=70)
+    title = models.CharField(max_length=70)
+    ingress = models.TextField()
+    hoveddel = models.TextField()
+    image_mellom = models.TextField(blank = True)
+    image_mellomtext = models.TextField(blank = True)
+    
+    
