85ad752242a8cea7d206c0e1cbd992db8822ca82
Sivaranjith1 <ranjith1210000@gmail.com>
2017-08-08 13:06:59 +0200

    major change

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class article(models.Model):
-    image_title = models.CharField(max_length=100)
-    image_text = models.CharField(max_length=70)
+class Article(models.Model):
+    #image_title = models.CharField(max_length=100)
+    #image_text = models.CharField(max_length=70)
     title = models.CharField(max_length=70)
-    ingress = models.TextField()
-    hoveddel = models.TextField()
-    image_mellom = models.TextField(blank = True)
-    image_mellomtext = models.TextField(blank = True)
-    
+    #date = models.DateTimeField()
+    #ingress = models.TextField(blank=True)
+    #hoveddel = models.TextField(blank=True)
+    #image_mellom = models.TextField(blank = True)
+    #image_mellomtext = models.TextField(blank = True)
+
+    def __str__(self):
+        return self.title
     
--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
-from . import views
+from django.views.generic import ListView, DetailView
+from news.models import Article
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', ListView.as_view(queryset=Article.objects.all(),
+                                template_name="news_home.html")),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Article,
+                                             template_name="news_article.html"))
 ]
